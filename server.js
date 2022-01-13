a3f7deeb676ce15ed04366b05dfd04f3f97a3a04
Felipe Monteiro <feliipemonteiro901@gmail.com>
2022-01-13 10:47:38 -0300

    add express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('batata')
+const express = require("express");
+const { resolve } = require("path");
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(process.env.port || 3000, (erro) => {
+  if (erro) {
+    return console.log(erro);
+  }
+  console.log("deu certo direitinho");
+});
