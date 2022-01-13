286bb8c1ac77618d5f5da32cbfacb52ff8e0a801
Felipe Monteiro <feliipemonteiro901@gmail.com>
2022-01-13 12:02:56 -0300

    atualizando rotas for heroku sem a /

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lol-stats-b.herokuapp.com/",
+  baseURL: "https://lol-stats-b.herokuapp.com",
 });
 
 export default api;
